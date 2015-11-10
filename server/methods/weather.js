74a60404450883469eb8c7d383eb48e515a220ee
Connor Hallett <connor.hallett@gmail.com>
2015-11-10 18:54:48 -0500

    Added weather information to layout

---
+++ server/methods/weather.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Connor on 11/10/2015.
+ */
+Meteor.methods({
+    'getWeatherData': function (lat, lng) {
+        return HTTP.call('get',
+            'http://api.wunderground.com/api/' + Meteor.settings.private.weather.key + '/hourly/q/' + lat + ',' + lng + '.json');
+    }
+});
