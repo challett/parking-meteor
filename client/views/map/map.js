356034dea6ab2f1055683e86eddae0978a5e9c81
Connor Hallett <connor.hallett@gmail.com>
2015-11-11 14:41:48 -0500

    Added animation to marker

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -11,17 +11,21 @@ Template.map.rendered = function () {
         navigator.geolocation.getCurrentPosition(function (pos) {
             var lat = pos.coords.latitude;
             var lng = pos.coords.longitude;
+
+            var trafficLayer = new google.maps.TrafficLayer();
+
+            var marker = new google.maps.Marker({
+                position: {lat: lat, lng: lng},
+                map: map,
+                title: 'You Are Here',
+                animation: google.maps.Animation.DROP
+            });
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 14,
                 center: {lat: lat, lng: lng}
             });
-            var trafficLayer = new google.maps.TrafficLayer();
             trafficLayer.setMap(map);
-            var marker = new google.maps.Marker({
-                position: {lat: lat, lng: lng},
-                map: map,
-                title: 'You Are Here'
-            });
+            marker.setMap(map);
         }, function (err) {
             console.warn(err)}, geooptions);
     }
