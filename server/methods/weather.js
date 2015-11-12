9579002188720a525a1df7360a79c3f9032e78fc
Connor Hallett <connor.hallett@gmail.com>
2015-11-11 22:01:19 -0500

    Added weather back

--- server/methods/weather.js
+++ server/methods/weather.js
@@ -3,8 +3,7 @@
  */
 Meteor.methods({
     'getWeatherData': function (lat, lng) {
-        //return HTTP.call('get',
-        //    'http://api.wunderground.com/api/' + Meteor.settings.private.weather.key + '/hourly/q/' + lat + ',' + lng + '.json');
-        return {}
+        return HTTP.call('get',
+            'http://api.wunderground.com/api/' + Meteor.settings.private.weather.key + '/hourly/q/' + lat + ',' + lng + '.json');
     }
 });
